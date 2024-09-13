3f0b1c6f303b86a19911766e8cf542e888ca943a
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-14 02:53:11 +0900

    changed to No.

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ const rl = readline.createInterface({
 
 rl.question("Enter a string: ", (input) => {
   console.log("Input: ", input);
-  console.log("# of total characters: ", countChars(input));
-  console.log("# of total vowels: ", countVowels(input));
-  console.log("# of total digits: ", countDigits(input));
+  console.log("No. of total characters: ", countChars(input));
+  console.log("No. of total vowels: ", countVowels(input));
+  console.log("No. of total digits: ", countDigits(input));
   rl.close();
 });
