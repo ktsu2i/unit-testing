142678a3da6b946952ea9630aa92d0ac3f8e9bcf
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-14 01:18:19 +0900

    added more test codes for countChars

--- test/countChars.test.ts
+++ test/countChars.test.ts
@@ -1,5 +1,46 @@
 import { countChars } from "../src/countChars";
 
-test("&quot;testing&quot; should return 7", () => {
+// all characters, no spaces
+test("'testing' should return 7", () => {
   expect(countChars("testing")).toBe(7);
+});
+
+// all characters, with spaces
+test("'this is testing' should return 13", () => {
+  expect(countChars("this is testing")).toBe(13);
+});
+
+// characters & symbols, no spaces
+test("'testing...!!' should return 12", () => {
+  expect(countChars("testing...!!")).toBe(12);
+});
+
+// all numbers, no spaces
+test("'12345' should return 5", () => {
+  expect(countChars("12345")).toBe(5);
+});
+
+// symbols & numbers, with spaces
+test("'1 + 2 = 3' should return 5", () => {
+  expect(countChars("1 + 2 = 3")).toBe(5);
+});
+
+// characters, numbers, & symbols, with spaces
+test("'<input id=3296 />' should return 15", () => {
+  expect(countChars("<input id=3296 />")).toBe(15);
+});
+
+// Only one space
+test("' ' should return 0", () => {
+  expect(countChars(" ")).toBe(0);
+});
+
+// Many spaces
+test("'    ' should return 0", () => {
+  expect(countChars("    ")).toBe(0);
+});
+
+// empty string
+test("'' should return 0", () => {
+  expect(countChars("")).toBe(0);
 });
