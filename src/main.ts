8cf2ee6b53b4c7828b5257ba5bd7f834fe34f7bc
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-14 02:49:13 +0900

    fixed typo

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ rl.question("Enter a string: ", (input) => {
   console.log("Input: ", input);
   console.log("# of total characters: ", countChars(input));
   console.log("# of total vowels: ", countVowels(input));
-  console.log("$ of total digits: ", countDigits(input));
+  console.log("# of total digits: ", countDigits(input));
   rl.close();
 });
