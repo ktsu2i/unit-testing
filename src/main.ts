89125e633ca8d15f4184f059e384a35bdd9700db
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-14 02:39:40 +0900

    added main.ts

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import * as readline from "readline";
+import { countChars } from "./countChars";
+import { countVowels } from "./countVowels";
+import { countDigits } from "./countDigits";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter a string: ", (input) => {
+  console.log("Input: ", input);
+  console.log("# of total characters: ", countChars(input));
+  console.log("# of total vowels: ", countVowels(input));
+  console.log("$ of total digits: ", countDigits(input));
+  rl.close();
+});
