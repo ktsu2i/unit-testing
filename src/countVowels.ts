41b92f81e7c8365e73b2cfb8db8d532082f64ec4
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-14 03:36:09 +0900

    changed to regrex in countVowels too

--- src/countVowels.ts
+++ src/countVowels.ts
@@ -1,10 +1,9 @@
 export function countVowels(s: string): number {
-  const vowels = ["a", "e", "i", "o", "u"];
   const chars = s.toLowerCase().split("");
   let count: number = 0;
 
   for (const c of chars) {
-    if (vowels.includes(c)) {
+    if (c.match(/[aeiou]/)) {
       count++;
     }
   }
