8cc12dd91e36e3ca05760306d262550d3f3e46d0
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-14 03:35:20 +0900

    changed to regrex

--- src/countDigits.ts
+++ src/countDigits.ts
@@ -1,10 +1,9 @@
 export function countDigits(s: string): number {
-  const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const chars = [...s];
   let count: number = 0;
 
   for (const c of chars) {
-    if (digits.includes(c)) {
+    if (c.match(/[0-9]/)) {
       count++;
     }
   }
